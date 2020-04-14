5d7ef70b02950f41bb7a0e980dd6e77d377f8e14
jstonedev <jstonedev521@gmail.com>
2020-04-13 22:46:29 -0400

    Set up router functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 import "./App.css";
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>App</h1>
-			</div>
+			<Fragment>
+				<Router>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/rooms" component={Rooms} />
+						<Route exact path="/rooms/:slug" component={SingleRoom} />
+						<Route component={Error} />
+					</Switch>
+				</Router>
+			</Fragment>
 		);
 	}
 }
